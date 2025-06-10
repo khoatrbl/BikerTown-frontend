246d8f7202c2849a57b0878e617171b97a851d37
Khoa <khoa.h0086881@gmail.com>
2025-06-10 15:58:44 +0700

    Deleted duplicated files

--- stop_create.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class StopCreate(BaseModel):
-    stop_name: str
--- todo_create.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class ToDoCreate(BaseModel):
-    stop_id: int
-    item_name: str
-    item_status: bool
-    item_order: int
