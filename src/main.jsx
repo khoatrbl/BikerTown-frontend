f5e93a2af4a7c8c9292030988b14d19b4dc016be
Khoa <khoa.h0086881@gmail.com>
2025-06-17 15:52:45 +0700

    Added authenticator

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ConfigProvider, App as AntdApp } from "antd";
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ConfigProvider>
-      <AntdApp>
-        <App />
-      </AntdApp>
-    </ConfigProvider>
+    <Authenticator>
+      <ConfigProvider>
+        <AntdApp>
+          <App />
+        </AntdApp>
+      </ConfigProvider>
+    </Authenticator>
   </React.StrictMode>
 );
