a1b8094f9f91745c00bb9460640aaec22ce57a49
Khoa <khoa.h0086881@gmail.com>
2025-06-17 16:01:52 +0700

    Minor authenticator fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ConfigProvider, App as AntdApp } from "antd";
-import ErrorBoundary from "./ErrorBoundary";
+import { Authenticator } from "@aws-amplify/ui-react";
+// import ErrorBoundary from "./ErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
