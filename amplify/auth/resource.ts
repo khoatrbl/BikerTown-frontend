9951b451d1c4b85e3a759c481d68c70341d626cb
Khoa <khoa.h0086881@gmail.com>
2025-08-07 06:42:07 +0700

    added amplify for deployment

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,17 @@
+import { defineAuth } from "@aws-amplify/backend";
+
+/**
+ * Define and configure your auth resource
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    // email: {
+    //   verificationEmailStyle: "CODE",
+    //   verificationEmailSubject: "Welcome to BikerTown",
+    //   verificationEmailBody: (createCode) =>
+    //     `Hello, please verify your email with the code: ${createCode()}`,
+    // },
+    email: true,
+  },
+});
