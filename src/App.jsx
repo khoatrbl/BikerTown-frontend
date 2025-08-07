bf844fef4dffcf78c3a8f0650f6cb88ee44f8620
Khoa <khoa.h0086881@gmail.com>
2025-08-07 12:36:37 +0700

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import AppRoutes from "./routes/routes";
 import "antd/dist/reset.css"; // Add this line for Ant Design styles
 import "./App.css";
 
+import { useEffect } from "react";
+import { Amplify } from "aws-amplify";
+
 function App() {
+  useEffect(() => {
+    const config = Amplify.getConfig();
+    console.log("Loaded config:", config);
+  }, []);
+
   return (
     <>
       <AppRoutes />
